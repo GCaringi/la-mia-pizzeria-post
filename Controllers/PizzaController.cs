f2dc9bc8211c24b7d25711c48bd4e3d662bcb6ef
GCaringi <giannicaringi94@gmail.com>
2022-10-05 12:58:04 +0200

    Initial commit

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,51 @@
+﻿using la_mia_pizzeria_post.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_post.Controllers;
+
+public class PizzaController : Controller
+{
+    
+    
+    private readonly List<Pizza> _pizzas = new()
+    {
+        new Pizza
+        {
+            Id = 1, Name = "Margherita", Description = "Pomodoro Mozzarella", Image = "margherita.png", Price = 4.00m
+        },
+        new Pizza
+        {
+            Id = 2, Name = "Diavola", Description = "Margherita + Salame", Image = "diavola.png", Price = 7.00m
+        },
+        new Pizza
+        {
+            Id = 3, Name = "Provola", Description = "Bianca con Provola", Image = "provola.png", Price = 6.00m
+        },
+        new Pizza
+        {
+            Id = 4, Name = "Paperino", Description = "Bianca con Patatine", Image = "paperino.png", Price = 9.00m
+        },
+        new Pizza
+        {
+            Id = 5, Name = "Romanina", Description = "Rossa aglio origano", Image = "romanina.png", Price = 6.00m
+        }
+
+    };
+
+    public IActionResult Index()
+    {
+        return View(_pizzas);
+    }
+
+    public IActionResult ShowById(int id)
+    {
+        Pizza? pizza = _pizzas.Find(x => x.Id == id);
+        
+        if (pizza == null)
+        {
+            return View("Error");
+        }
+        
+        return View(pizza);
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,10 @@
+﻿namespace la_mia_pizzeria_post.Models;
+
+public class Pizza
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? Image { get; set; }
+    public decimal Price { get; set; }
+}
